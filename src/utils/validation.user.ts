c2f8faa85401b1fda5ed5ba51d83d68caa1c6cc5
Richard Tekula <barspin4499@gmail.com>
2025-07-09 12:05:22 +0200

    Update validation.user.ts
    fix role id from string to number

--- src/utils/validation.user.ts
+++ src/utils/validation.user.ts
@@ -1,9 +1,9 @@
-import { z } from "zod";
+import { z } from 'zod';
 
-export const insertUserSchema = z.object({
+const insertUserSchema = z.object({
   email: z.string().email(),
-  password: z.string().min(6),
-  first_name: z.string().min(1),
-  last_name: z.string().min(1),
-  role_id: z.number().int(),
+  password: z.string(),
+  first_name: z.string(),
+  last_name: z.string(),
+  role_id: z.preprocess((val) => Number(val), z.number()),
 });
